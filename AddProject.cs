dc9a5bffd399e36d0fa287d766434cadb42672ab
chidozie chukwugbo <chukwugboc@gmail.com>
2022-04-12 13:53:04 +0100

    Add project files.

---
+++ AddProject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace classes
+{
+    public class AddProject
+    {
+        string ProjectName;
+        string Status;
+        string StartDate;
+        string EndDate;
+        string Assignee;
+
+        public AddProject(string projectName, string status, string startDate, string endDate, string assignee)
+        {
+            ProjectName = projectName;
+            Status = status;
+            StartDate = startDate;
+            EndDate = endDate;
+            Assignee = assignee;
+
+        }
+
+        public string printProject()
+        {
+
+            return string.Format($"Hi {Assignee}, you have been assigned the project {ProjectName} starting today {StartDate} till {EndDate}. Oce completed, please change the project status to {Status}");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System;
+
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, startDate, endDate, status, assignee;
+            Console.Write("Enter project name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter project start date: ");
+            startDate = Console.ReadLine();
+
+            Console.Write("Enter project end date: ");
+            endDate = Console.ReadLine();
+
+            Console.Write("Enter project status: ");
+            status = Console.ReadLine();
+
+            Console.Write("Who is the project assigned to? : ");
+            assignee = Console.ReadLine();
+
+            AddProject addProject = new AddProject(name, startDate, endDate, status, assignee);
+
+            Console.WriteLine(addProject.printProject());
+            Console.ReadLine();
+
+        }
+    }
+}
